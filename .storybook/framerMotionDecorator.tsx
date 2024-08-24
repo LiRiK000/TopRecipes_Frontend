9f5072ba9ffcf40066686229269b431ddd0c3f64
LiRiK <ggevelurl@gmail.com>
2024-08-24 17:39:40 +0300

    chore: Loki was removed
    Loki was removed due to animation issues in the storybook

--- .storybook/framerMotionDecorator.tsx
+++ .storybook/framerMotionDecorator.tsx
@@ -3,9 +3,6 @@ import LazyMotionProvider from '../src/components/Providers/LazyMotionProvider'
 import React from 'react'
 
 export const framerMotionDecorator: Decorator = Story => {
-  if (process.env.LOKI === 'true') {
-    document.body.classList.add('disable-animations')
-  }
   return (
     <LazyMotionProvider>
       <Story />
